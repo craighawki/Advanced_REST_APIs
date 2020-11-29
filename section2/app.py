af0caadc6a849dea8309164c919acabe11106458
Hawkins, Craig <craig.A.hawkins@ge.com>
2020-11-29 12:37:50 -0800

    From using vanilla Marshmallow

--- section2/app.py
+++ section2/app.py
@@ -7,6 +7,7 @@ from db import db
 from blacklist import BLACKLIST
 from resources.user import UserRegister, UserLogin, User, TokenRefresh, UserLogout, CraigInfo
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
@@ -34,6 +35,8 @@ jwt = JWTManager(app)
 def check_if_token_in_blacklist(decrypted_token):
     return decrypted_token["jti"] in BLACKLIST
 
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(StoreList, "/stores")
 api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemList, "/items")
 api.add_resource(UserRegister, "/register")
--- section2/resources/store.py
+++ section2/resources/store.py
@@ -1,17 +1,21 @@
 from flask_restful import Resource
 from models.store import StoreModel
+from schemas.store import StoreSchema
 
 NAME_ALREADY_EXISTS = "A store with name '{}' already exists."
 ERROR_INSERTING = "An error occurred while inserting the store."
 STORE_NOT_FOUND = "Store not found."
 STORE_DELETED = "Store deleted."
 
+store_schema = StoreSchema()
+store_list_schema = StoreSchema(many=True)
+
 class Store(Resource):
     @classmethod
     def get(cls, name: str):
         store = StoreModel.find_by_name(name)
         if store:
-            return store.json()
+            return store_schema.dump(store), 200
         return {"message": STORE_NOT_FOUND}, 404
 
     @classmethod
@@ -21,13 +25,13 @@ class Store(Resource):
                 "message": NAME_ALREADY_EXISTS.format(name)
             }, 400
 
-        store = StoreModel(name)
+        store = StoreModel(name=name)
         try:
             store.save_to_db()
         except:
             return {"message": ERROR_INSERTING}, 500
 
-        return store.json(), 201
+        return store_schema.dump(store), 201
 
     @classmethod
     def delete(cls, name: str):
@@ -41,4 +45,4 @@ class Store(Resource):
 class StoreList(Resource):
     @classmethod
     def get(cls):
-        return {"stores": [x.json() for x in StoreModel.find_all()]}
+        return {"stores": store_list_schema.dump(StoreModel.find_all())}, 200
